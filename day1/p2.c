d28b39e91631b0c5d363218ad5f7982e6b1564ee
Bedanta Chakraborty <bedantachakraborty200555@gmail.com>
2025-08-16 15:36:55 +0000

    day 1, problem-2 with question

---
+++ day1/p2.c
@@ -0,0 +1,17 @@
+/*
+Write a C program to add floating number
+*/
+
+#include<stdio.h>
+
+int  main(){
+    
+    float a = 5.3;
+    float b = 6.7;
+    float c = a+b;
+    printf("%f\n", c);
+
+    return 0;
+}
+
+
