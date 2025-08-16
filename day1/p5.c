8fc0d4214736debc0e8cd78b67d904593627eece
Bedanta Chakraborty <bedantachakraborty200555@gmail.com>
2025-08-16 17:49:21 +0000

    day1, problem-5 with question

---
+++ day1/p5.c
@@ -0,0 +1,10 @@
+/*
+Write a C program to find the ASCII value of a charecter
+*/
+
+#include<stdio.h>
+int main(){
+    char c = 'G';
+    printf("The ASCII value of %c = %d\n", c, c);
+    return 0;
+}
