264fa052d9c544e8c26f7714d545061a9da52781
Bedanta Chakraborty <bedantachakraborty200555@gmail.com>
2025-08-16 16:22:00 +0000

    day1, problem-2 with question

--- day1/p2.c
+++ day1/p2.c
@@ -5,11 +5,11 @@ Write a C program to add floating number
 #include<stdio.h>
 
 int  main(){
-    
+
     float a = 5.3;
     float b = 6.7;
     float c = a+b;
-    printf("%f\n", c);
+    printf("The value of %f+%f = %f\n", a, b, c);
 
     return 0;
 }
