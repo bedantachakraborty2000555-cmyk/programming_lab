7c2538e8290cc79a079bf0a284d0f412e2e4be60
Bedanta Chakraborty <bedantachakraborty200555@gmail.com>
2025-08-16 17:33:37 +0000

    day1, program-4 with question

---
+++ day1/p4.c
@@ -0,0 +1,23 @@
+/*
+Write a C program to print multiplication table without using loop
+*/
+
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter a number: ");
+    scanf("%d", &a);
+    printf("Multiplication table of %d: \n", a);
+    printf("%d\n",a*1);
+    printf("%d\n",a*2);
+    printf("%d\n",a*3);
+    printf("%d\n",a*4);
+    printf("%d\n",a*5);
+    printf("%d\n",a*6);
+    printf("%d\n",a*7);
+    printf("%d\n",a*8);
+    printf("%d\n",a*9);
+    printf("%d\n",a*10);
+    return 0;
+    
+}
