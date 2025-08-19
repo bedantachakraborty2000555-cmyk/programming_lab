4c2d9dd65bfbc64852a30e505cfa5e4486590df8
Bedanta Chakraborty <bedantachakraborty200555@gmail.com>
2025-08-19 18:37:52 +0000

    day2, p1 with question

---
+++ day2/p1.c
@@ -0,0 +1,24 @@
+/*
+write a C program to calculate simple interest
+*/
+
+#include<stdio.h>
+
+int main(){
+    float p, r, t, i;
+    printf("Enter principle amount: ");
+    scanf("%f", &p);
+
+    printf("Enter rate of interest(in %% per annum): ");
+    scanf("%f",&r);
+
+    printf("Enter the time (in year): ");
+    scanf("%f", &t);
+
+    i=(p*r*t)/100;
+
+    printf(" The simple interest is  %f\n ",i);
+    
+    return 0;
+
+}
