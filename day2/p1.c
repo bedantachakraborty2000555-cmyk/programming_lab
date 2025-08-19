8d0e636d2ac555a3b871d5cfc13f1d6908e3c0af
Bedanta Chakraborty <bedantachakraborty200555@gmail.com>
2025-08-19 19:03:10 +0000

    day2,problem-1 with question

--- day2/p1.c
+++ day2/p1.c
@@ -5,7 +5,9 @@ write a C program to calculate simple interest
 #include<stdio.h>
 
 int main(){
+    
     float p, r, t, i;
+
     printf("Enter principle amount: ");
     scanf("%f", &p);
 
@@ -17,7 +19,7 @@ int main(){
 
     i=(p*r*t)/100;
 
-    printf(" The simple interest is  %f\n ",i);
+    printf(" The simple interest is  %f\n",i);
     
     return 0;
 
