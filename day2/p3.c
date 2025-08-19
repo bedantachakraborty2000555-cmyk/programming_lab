931fb77bec0f04910e4770eaf40708dc2735fcd1
Bedanta Chakraborty <bedantachakraborty200555@gmail.com>
2025-08-19 19:38:17 +0000

    day2, problem-3 with question

---
+++ day2/p3.c
@@ -0,0 +1,20 @@
+/*
+Write a C program to convert Fahrenheit to celsius the Celcius to fahrenheit
+*/
+
+#include<stdio.h>
+int main(){
+    float C, F;
+
+    printf(" Enter the Celcius :");
+    scanf("%f" ,&C);
+
+    printf(" The convertion into Fahrenheit is: %f\n",((9*C/5)+32));
+
+    printf(" Enter Fahrenheit :");
+    scanf("%f",&F);
+
+    printf(" The convertion into Celcius is : %f\n",((F-32)*5/9));
+
+    return 0;
+}
