4c3ed4e53dc7b5acfc44f7f49016871e8de01518
Bedanta Chakraborty <bedantachakraborty200555@gmail.com>
2025-08-19 18:56:11 +0000

    day2,problem-2 with question

---
+++ day2/p2.c
@@ -0,0 +1,14 @@
+/*
+Write a C program  to calculate area of a circle 
+*/
+
+#include<stdio.h>
+
+int main(){
+    float r ;
+    printf("Enter the radius of the circle: ");
+    scanf("%f",&r);
+
+    printf("The area of the circle is %f\n", r*r*3.14);
+    return 0;
+}
