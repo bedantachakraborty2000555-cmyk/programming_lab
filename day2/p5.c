0519fe01bec8bca667a44246537b3e9aa90425d7
Bedanta Chakraborty <bedantachakraborty200555@gmail.com>
2025-08-20 18:51:23 +0000

    day2,problem-5 with question

--- day2/p5.c
+++ day2/p5.c
@@ -1,10 +1,10 @@
 /*
-Write a C program to swap two numbers  using third variable
+Write a C program to swap two numbers without using third variable
   */
 
 #include<stdio.h>
 int main(){
-    int n1, n2, n3;
+    int n1, n2;
 
     printf("Enter the first no : ");
     scanf("%d", &n1);
@@ -16,9 +16,9 @@ int main(){
     printf(" first number = %d\n",n1 );
     printf(" second number = %d\n",n2);
 
-    n3=n1;
-    n1=n2;
-    n2=n3;
+    n1=n1+n2;
+    n2=n1-n2;
+    n1=n1-n2;
 
     printf("After swapping :\n");
     printf(" first number = %d\n",n1 );
